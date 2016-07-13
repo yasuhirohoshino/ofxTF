535810f4cb5c40404d93ff1d2f6a7dbbd9095178
yasuhirohoshino <yasuhiro.hoshino.0122@gmail.com>
2016-07-13 23:39:59 +0900

    first commit

---
+++ src/ofxTF.cpp
@@ -0,0 +1,93 @@
+#include "ofxTF.h"
+
+void ofxTF::setup(int total)
+{
+	this->total = total;
+}
+
+void ofxTF::generate(string shaderPath)
+{
+	loadShader(shaderPath);
+
+	glGenTransformFeedbacks(2, feedback);
+
+	for (int i = 0; i<2; i++) {
+		for (int j = 0; j < tfObjects.size(); j++) {
+			vbo[i].setAttributeBuffer(shader.getAttributeLocation(tfObjects[j].inLocationName), 
+				tfObjects[j].bufferObject[i], 
+				tfObjects[j].num,
+				0);
+		}
+
+		glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, feedback[i]);
+
+		for (int j = 0; j < tfObjects.size(); j++) {
+			glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, j, tfObjects[j].bufferObject[i].getId());
+		}
+
+		glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
+	}
+
+	frame = 1;
+}
+
+void ofxTF::loadShader(string shaderPath)
+{
+	shader.setupShaderFromFile(GL_VERTEX_SHADER, shaderPath);
+	for (int i = 0; i < tfObjects.size(); i++) {
+		feedbackVaryings.push_back(tfObjects[i].outLocationName);
+	}
+	glTransformFeedbackVaryings(shader.getProgram(), tfObjects.size(), &feedbackVaryings[0], GL_SEPARATE_ATTRIBS);
+	shader.linkProgram();
+}
+
+void ofxTF::begin()
+{
+	frame = 1 - frame;
+	shader.begin();
+}
+
+void ofxTF::end()
+{
+	glEnable(GL_RASTERIZER_DISCARD);
+	glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, feedback[frame]);
+	glBeginTransformFeedback(GL_POINTS);
+	vbo[1 - frame].draw(GL_POINTS, 0, total);
+	glEndTransformFeedback();
+	glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
+	glDisable(GL_RASTERIZER_DISCARD);
+	shader.end();
+}
+
+void ofxTF::addBufferObject(string inLocationName, string outLocationName, int num, int internalFormat, float * values)
+{
+	TFObject object;
+	object.inLocationName = inLocationName;
+    int length = outLocationName.length();
+    char* name = new char[length + 1];
+    memcpy(name, outLocationName.c_str(), length + 1);
+	object.outLocationName = name;
+	object.num = num;
+	for (int i = 0; i < 2; i++) {
+		object.bufferObject[i].allocate();
+		object.bufferObject[i].bind(GL_TEXTURE_BUFFER);
+		object.bufferObject[i].setData(sizeof(float) * total * object.num, values, GL_STREAM_DRAW);
+		object.texBuffer[i].allocateAsBufferTexture(object.bufferObject[i], internalFormat);
+	}
+	tfObjects.push_back(object);
+}
+
+ofTexture * ofxTF::getTexture(int index)
+{
+	return  &tfObjects[index].texBuffer[frame];
+}
+
+ofShader * ofxTF::getShader()
+{
+	return &shader;
+}
+
+ofVbo * ofxTF::getVbo()
+{
+	return &vbo[frame];
+}
---
+++ src/ofxTF.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ofMain.h"
+
+struct TFObject {
+	string inLocationName;
+	char* outLocationName;
+	int num;
+	ofTexture texBuffer[2];
+	ofBufferObject bufferObject[2];
+};
+
+class ofxTF {
+public:
+	void setup(int total);
+	void generate(string shaderPath);
+	void loadShader(string shaderPath);
+	void begin();
+	void end();
+	void addBufferObject(string inLocationName, string outLocationName, int num, int internalFormat, float * values);
+	ofTexture* getTexture(int index);
+	ofShader* getShader();
+	ofVbo* getVbo();
+
+private:
+	GLuint feedback[2];
+	ofVbo vbo[2];
+	int frame;
+	vector<TFObject> tfObjects;
+	int total;
+	ofShader shader;
+	vector<char*> feedbackVaryings;
+};
